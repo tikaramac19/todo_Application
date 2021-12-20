5941625b95b036f1bb5994890ba1eb09b03f5371
Tikaram_Ac <tikaramac19@gmail.com>
2021-12-20 22:25:49 +0545

    todo application

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { useState } from "react";
+import ToDoList from "./toDoList";
 
 function App() {
+  const [text, setText] = useState("");
+  const [items, setItems] = useState([""]);
+
+  const showtext = (event) => {
+    setText(event.target.value);
+  };
+
+  const listOfItem = () => {
+    setItems((oldItems) => {
+      return [...oldItems, text];
+    });
+
+    setText("");
+  };
+
+  const deleteItem = () => {
+    console.log("deleted items");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <div className="centerdiv">
+          <h2 className="header">ToDo List</h2>
+
+          <br />
+
+          <input
+            type="text"
+            placeholder="add a Items"
+            value={text}
+            onChange={showtext}
+          />
+          <button onClick={listOfItem} className="bnt">
+            {" "}
+            +{" "}
+          </button>
+
+          <ol>
+            {items.map((itemvalue, index) => {
+              return (
+                <ToDoList
+                  key={index}
+                  id={index}
+                  textt={itemvalue}
+                  onSelect={deleteItem}
+                />
+              );
+            })}
+          </ol>
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/toDoList.js
@@ -0,0 +1,13 @@
+const ToDoList = (props) => {
+
+  return (
+    <>
+      <div className="todo">
+
+        <li>{props.textt}</li>
+      </div>
+    </>
+  );
+};
+
+export default ToDoList;
